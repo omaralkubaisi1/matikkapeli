4e6700726ae6d5ea752465a9e97a1b40326366fa
omaralkubaisi1 <c3alom00@students.oamk.fi>
2024-02-06 14:24:08 +0200

    Math game

---
+++ js/functions.js
@@ -0,0 +1,38 @@
+let rand_num1 = 0
+let rand_num2 = 0
+let correctCount = 0
+let incorrectCount = 0
+
+const updateScore = () => {
+    document.querySelector('#correctCount').textContent = `Correct answers: ${correctCount}`;
+    document.querySelector('#incorrectCount').textContent = `Incorrect answers: ${incorrectCount}`;
+}
+
+const getRandomIntNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+const randomizeNumbers  = () => {
+    rand_num1 = getRandomIntNumberInRange(1, 10)
+    rand_num2 = getRandomIntNumberInRange(1, 10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+    updateScore()
+});
+document.querySelector('button').addEventListener('click', () => {
+    const answer  = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer) {
+        alert('Correct!')
+        correctCount++;
+    } else {
+        alert('Incorrect!')
+        incorrectCount++;
+    }
+    randomizeNumbers()
+    document.querySelector('input').value=''
+    updateScore();
+})
